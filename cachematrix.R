8df1b0df6f77c0a21ec27e59f8ab088e5e8105b3
elegantcoderM <manudhundi@yahoo.com>
2015-05-22 01:00:23 -0700

    Added comment to cacheSolve() function

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## Returns the inverse of the matrix cached in obj x
+## If the inverse is already computed (and cached)
+## then the cached value is returned, otherwise
+## inverse is calculated using solve()
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
